129e09720e8820cc8e5cf33f448944a5e03bcbec
Chase Colman <chase@colman.io>
2015-01-18 16:39:44 -0500

    Remove gvim settings

--- setup.sh
+++ setup.sh
@@ -5,7 +5,5 @@ git submodule init && git submodule update
 popd
 ln -si $abspath/vimrc.before ~/.vimrc.before
 ln -si $abspath/vimrc.after ~/.vimrc.after
-ln -si $abspath/gvimrc.before ~/.gvimrc.before
-ln -si $abspath/gvimrc.after ~/.gvimrc.after
 [ -d ~/.janus ] && rm -rfI ~./janus
 ln -si $abspath/janus ~/.janus
