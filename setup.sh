9cf2da91f2008e2899fa4d48fc32a04e18e75a5b
Chase Colman <chase@colman.io>
2015-01-17 20:59:04 -0500

    Make setup helper a little more helpful

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-ln -si vimrc.before ~/.vimrc.before
-ln -si vimrc.after ~/.vimrc.after
-ln -si gvimrc.before ~/.gvimrc.before
-ln -si gvimrc.after ~/.gvimrc.after
-ln -si janus ~/.janus
+abspath=`pwd`
+pushd -q janus
+git submodule init && git submodule update
+popd -q
+ln -si $abspath/vimrc.before ~/.vimrc.before
+ln -si $abspath/vimrc.after ~/.vimrc.after
+ln -si $abspath/gvimrc.before ~/.gvimrc.before
+ln -si $abspath/gvimrc.after ~/.gvimrc.after
+[ -e ~/.janus ] && rm -rfI ~./janus; ln -s $abspath/janus ~/.janus
