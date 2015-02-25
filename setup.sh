5cd4c9756bfcfacb1230c1a0a4c019a8e596356b
Chase Colman <chase@colman.io>
2015-02-24 20:00:42 -0500

    Better setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,18 @@
-#!/bin/sh
+#!/usr/bin/zsh
 abspath=`pwd`
-ln -si $abspath/vimrc ~/.vimrc
-[ -d ~/.vim ] && rm -rfI ~/.vim
-ln -si $abspath/vim ~/.vim
-[ -d ~/.zsh-custom ] && rm -rfI ~/zsh-custom
-ln -si $abspath/zsh-custom ~/.zsh-custom
-ln -si $abspath/zshrc ~/.zshrc
-
-vim +NeoBundleInstall +qall
+echo -n "Setup vim files? (.vim and .vimrc will be removed) [N/y] " && read yesno
+if [[ "$yesno:l" -eq "y" ]] {
+    [ -e ~/.vimrc ] && rm ~/.vimrc
+    ln -s $abspath/vimrc ~/.vimrc
+    [ -d ~/.vim ] && rm -rf ~/.vim
+    ln -s $abspath/vim ~/.vim
+    git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+    vim +NeoBundleInstall +qall
+}
+echo -n "Setup zsh files? (.vim and .vimrc will be removed) [N/y] " && read yesno
+if [[ "$yesno:l" -eq "y" ]] {
+    [ -d ~/.zsh-custom ] && rm -rf ~/zsh-custom
+    ln -s $abspath/zsh-custom ~/.zsh-custom
+    [ -e ~/.zshrc ] && rm ~/.zshrc
+    ln -s $abspath/zshrc ~/.zshrc
+}
